29a60a014e53e7250793f1f530b9d35eb093d443
alexychen2002 <46267426+alexychen2002@users.noreply.github.com>
2019-02-10 14:25:08 -0500

    Create BinaryClock.java

---
+++ BinaryClock.java
@@ -0,0 +1,12 @@
+package binaryclock;
+
+public class BinaryClock {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Window clock = new Window();
+    }
+    
+}
